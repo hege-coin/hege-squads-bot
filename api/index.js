6a5ad0cb9580bd8108d5f6d70bc13de5a9ce95c8
dan <dkowalsky7@gmail.com>
2025-01-05 19:54:06 -0500

    update old code

--- api/index.js
+++ api/index.js
@@ -5,25 +5,13 @@ const {
   extractListingPrice,
   extractSeller,
 } = require("./extractListingPrice.js");
-const TwitterController = require('../controllers/X.controller');
+// const TwitterController = require('../controllers/X.controller');
 const { addCommas } = require("../helpers/addCommas.js");
 const CoingeckoController = require("../controllers/Coingecko.controller.js");
 
 const TELEGRAM_BOT_TOKEN = process.env.BOT_TOKEN;
 const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 const X_USER = process.env.X_USER;
-// const PROD = Boolean(process.env.PROD);
-// console.log("PROD", PROD);
-// // Main
-// if (PROD) {
-//   console.log("MAIN_CHAT", process.env.MAIN_CHAT);
-//   var TELEGRAM_CHAT_ID = process.env.MAIN_CHAT;
-// }
-// // Test
-// else {
-//   console.log("TEST_CHAT", process.env.TEST_CHAT);
-//   var TELEGRAM_CHAT_ID = process.env.TEST_CHAT;
-// }
 
 const HELIUS_API_KEY = process.env.HELIUS_KEY;
 const HELIUS_RPC_URL = `https://mainnet.helius-rpc.com/?api-key=${HELIUS_API_KEY}`;
@@ -33,167 +21,168 @@ const ROYALTY_FEE = 0.08;
 let jsonData;
 
 // Read the JSON data once and store it in the variable
-async function initializeJson() {
-  try {
-    const filePath = path.join(__dirname, "rarity.json");
-    const data = await fs.readFile(filePath, "utf8");
-    jsonData = JSON.parse(data);
-    console.log("JSON Data Loaded");
-  } catch (error) {
-    console.error("Error reading JSON file:", error);
-  }
-}
+// async function initializeJson() {
+//   try {
+//     const filePath = path.join(__dirname, "rarity.json");
+//     const data = await fs.readFile(filePath, "utf8");
+//     jsonData = JSON.parse(data);
+//     console.log("JSON Data Loaded");
+//   } catch (error) {
+//     console.error("Error reading JSON file:", error);
+//   }
+// }
 
 // Vercel API handler
 module.exports = async function main(req, res) {
   if (req.method === "POST") {
     const requestBody = req.body;
-
-    if (requestBody[0].type !== "TRANSFER") {
-      console.log(requestBody[0].signature);
-
-      const transactionData = await checkTransactionStatus(
-          requestBody[0].signature
-      );
-      const action = extractTransactionType(transactionData.meta.logMessages);
-      console.log(action);
-      // console.log("Transaction confirmed:", JSON.stringify(transactionData, null, 2));
-      // console.log("Transaction confirmed:", JSON.stringify(jsonData, null, 2));
-      const Transfertimestamp = new Date(
-          requestBody[0].timestamp * 1000
-      ).toLocaleString();
-      const Transfersignature = `https://solana.fm/tx/${requestBody[0].signature}`;
-
-      let url, mp, index, listingPriceInLamports;
-      let origin;
-      if (
-          requestBody[0].instructions[2].programId ===
-          "M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K"
-      ) {
-        // Handle Magic Eden
-        origin = "ME";
-        url = `https://magiceden.us/marketplace/hegends?activeTab=myItems&solItemDetailsModal=`;
-        mp = "Magic Eden";
-        index = 4;
-        listingPriceInLamports = await extractListingPrice(
-            requestBody,
-            action,
-            "ME"
-        );
-      } else if (
-          requestBody[0].instructions[2].programId ===
-          "TCMPhJdwDryooaGtiocG1u3xcYbRpiJzb283XfCZsDp"
-      ) {
-        // Handle Tensor
-        origin = "Tensor";
-        url = `https://www.tensor.trade/item/`;
-        mp = "Tensor";
-        index = action === "Delist" || action === "Listing" ? 0 : 2;
-        listingPriceInLamports = await extractListingPrice(
-            requestBody,
-            action,
-            "Tensor"
-        );
-      } else {
-        url = "";
-        mp = "";
-        index = 4;
-      }
-
-      const priceDecimals = origin === "ME" ? 3 : 2;
-      const NFTmintAddress = requestBody[0].instructions[2]["accounts"][index];
-      const mintUrl = `https://solana.fm/address/${NFTmintAddress}`;
-      const asset = await getAssetImageUrl(NFTmintAddress);
-      const im = asset.content.links.image;
-      const name = asset.content.metadata.name;
-      const desc = asset.content.metadata.description;
-      url += NFTmintAddress;
-
-      const ranking = jsonData.result.data.items.find(
-          (obj) => obj.mint === NFTmintAddress
-      );
-      const rank = ranking.rank;
-      const tier = getTextForRange(rank);
-
-      const listingPriceInSol = (listingPriceInLamports / 1000000000).toFixed(
-          priceDecimals
-      );
-
-      const takerFeePercentage = origin === "ME" ? 0.025 : 0.015;
-
-      const takerFee = Number(
-          (listingPriceInSol * takerFeePercentage).toFixed(priceDecimals)
-      );
-
-      const royaltyFee = Number(
-          (listingPriceInSol * ROYALTY_FEE).toFixed(priceDecimals)
-      );
-
-      const priceWithRoyalties = (
-          Number(listingPriceInSol) +
-          takerFee +
-          royaltyFee
-      ).toFixed(priceDecimals);
-
-      const {seller, abbreviatedSeller} = extractSeller(requestBody);
-
-      const sellerUrl = `https://solana.fm/address/${seller}`;
-
-      const solPrice = await CoingeckoController.fetchSolPrice();
-
-      const priceWithRoyaltiesInUSD = (
-          Number(priceWithRoyalties) * solPrice
-      ).toFixed(0);
-
-      const hegends = await CoingeckoController.getHegends();
-
-      const floorPrice = hegends.floor_price.native_currency;
-      const marketCapSol = Math.round(floorPrice * 2222);
-
-      const floorPriceInUSD = (floorPrice * solPrice).toFixed(0);
-      const marketCapInUSD = addCommas(Math.round(marketCapSol * solPrice));
-
-      const volume24h = hegends.volume_24h.native_currency;
-      const volume24hInUSD = (volume24h * solPrice).toFixed(0);
-
-      let messageToSendTransfer;
-      // const messageToSendTransfer = `
-      // <b>New ${action}!</b>\n\n<b>${name}</b>\n${desc}\n\n<b>Market:</b> <a href='${url}'>${mp}</a>\n<b>Rank: </b>${rank}\n<b>Tier: </b>${tier}\n<b>Price: </b>${priceWithRoyalties} SOL ($${priceWithRoyaltiesInUSD})\n\n<b>Floor Price: </b>${floorPrice.toFixed(2)} SOL ($${floorPriceInUSD})\n<b>Volume 24h: </b>${volume24h} SOL ($${volume24hInUSD})\n<b>Market Cap: </b>${marketCapSol.toFixed(0)} SOL ($${marketCapInUSD})\n\n<a href='${Transfersignature}'>TX</a> | <a href='${mintUrl}'>Mint</a> | <a href='${sellerUrl}'>Seller</a>`;
-
-      if (action === "Listing") {
-        // await sendToTelegramNFT(messageToSendTransfer, im);
-      } else if (action === "Sell") {
-
-        const data = {
-          uri: im,
-          text: `New Hegend Buy!\n\n${name}\n${desc}\n\nRank: ${rank}\nTier: ${tier}\nPrice: ${priceWithRoyalties} SOL (\$${priceWithRoyaltiesInUSD})\n${url}`,
-        };
-
-        try {
-          const tweet = await TwitterController.postToTwitter(data);
-          console.log('Tweet posted:', tweet);
-          console.log('Tweet posted:', tweet.data.id);
-          const tweetURL = `https://x.com/${X_USER}/status/${tweet.data.id}`
-          messageToSendTransfer = `<b>New ${action}!</b>\n\n<b>${name}</b>\n${desc}\n\n<b>Raid Tweet</b>\n${tweetURL}\n\n<b>Market:</b> <a href='${url}'>${mp}</a>\n<b>Rank: </b>${rank}\n<b>Tier: </b>${tier}\n<b>Price: </b>${priceWithRoyalties} SOL ($${priceWithRoyaltiesInUSD})\n\n<b>Floor Price: </b>${floorPrice.toFixed(2)} SOL ($${floorPriceInUSD})\n<b>Volume 24h: </b>${volume24h} SOL ($${volume24hInUSD})\n<b>Market Cap: </b>${marketCapSol.toFixed(0)} SOL ($${marketCapInUSD})\n\n<a href='${Transfersignature}'>TX</a> | <a href='${mintUrl}'>Mint</a> | <a href='${sellerUrl}'>Seller</a>`;
-
-        } catch (error) {
-          messageToSendTransfer = `
-      <b>New ${action}!</b>\n\n<b>${name}</b>\n${desc}\n\n<b>Market:</b> <a href='${url}'>${mp}</a>\n<b>Rank: </b>${rank}\n<b>Tier: </b>${tier}\n<b>Price: </b>${priceWithRoyalties} SOL ($${priceWithRoyaltiesInUSD})\n\n<b>Floor Price: </b>${floorPrice.toFixed(2)} SOL ($${floorPriceInUSD})\n<b>Volume 24h: </b>${volume24h} SOL ($${volume24hInUSD})\n<b>Market Cap: </b>${marketCapSol.toFixed(0)} SOL ($${marketCapInUSD})\n\n<a href='${Transfersignature}'>TX</a> | <a href='${mintUrl}'>Mint</a> | <a href='${sellerUrl}'>Seller</a>`;
-
-          console.error('X Error:', error);
-        }
-
-        if (priceWithRoyalties >= 1) {
-          await sendToTelegramNFT(messageToSendTransfer, im);
-        }
-
-      } else {
-        console.log("Transfer Transaction:");
-      }
-    }
-    res.status(200).send("Logged POST request body.");
-  } else {
-    res.status(405).send("Method not allowed.");
+    console.log(requestBody);
+    //   if (requestBody[0].type !== "TRANSFER") {
+    //     console.log(requestBody[0].signature);
+    //
+    //     const transactionData = await checkTransactionStatus(
+    //         requestBody[0].signature
+    //     );
+    //     const action = extractTransactionType(transactionData.meta.logMessages);
+    //     console.log(action);
+    //     // console.log("Transaction confirmed:", JSON.stringify(transactionData, null, 2));
+    //     // console.log("Transaction confirmed:", JSON.stringify(jsonData, null, 2));
+    //     const Transfertimestamp = new Date(
+    //         requestBody[0].timestamp * 1000
+    //     ).toLocaleString();
+    //     const Transfersignature = `https://solana.fm/tx/${requestBody[0].signature}`;
+    //
+    //     let url, mp, index, listingPriceInLamports;
+    //     let origin;
+    //     if (
+    //         requestBody[0].instructions[2].programId ===
+    //         "M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K"
+    //     ) {
+    //       // Handle Magic Eden
+    //       origin = "ME";
+    //       url = `https://magiceden.us/marketplace/hegends?activeTab=myItems&solItemDetailsModal=`;
+    //       mp = "Magic Eden";
+    //       index = 4;
+    //       listingPriceInLamports = await extractListingPrice(
+    //           requestBody,
+    //           action,
+    //           "ME"
+    //       );
+    //     } else if (
+    //         requestBody[0].instructions[2].programId ===
+    //         "TCMPhJdwDryooaGtiocG1u3xcYbRpiJzb283XfCZsDp"
+    //     ) {
+    //       // Handle Tensor
+    //       origin = "Tensor";
+    //       url = `https://www.tensor.trade/item/`;
+    //       mp = "Tensor";
+    //       index = action === "Delist" || action === "Listing" ? 0 : 2;
+    //       listingPriceInLamports = await extractListingPrice(
+    //           requestBody,
+    //           action,
+    //           "Tensor"
+    //       );
+    //     } else {
+    //       url = "";
+    //       mp = "";
+    //       index = 4;
+    //     }
+    //
+    //     const priceDecimals = origin === "ME" ? 3 : 2;
+    //     const NFTmintAddress = requestBody[0].instructions[2]["accounts"][index];
+    //     const mintUrl = `https://solana.fm/address/${NFTmintAddress}`;
+    //     const asset = await getAssetImageUrl(NFTmintAddress);
+    //     const im = asset.content.links.image;
+    //     const name = asset.content.metadata.name;
+    //     const desc = asset.content.metadata.description;
+    //     url += NFTmintAddress;
+    //
+    //     const ranking = jsonData.result.data.items.find(
+    //         (obj) => obj.mint === NFTmintAddress
+    //     );
+    //     const rank = ranking.rank;
+    //     const tier = getTextForRange(rank);
+    //
+    //     const listingPriceInSol = (listingPriceInLamports / 1000000000).toFixed(
+    //         priceDecimals
+    //     );
+    //
+    //     const takerFeePercentage = origin === "ME" ? 0.025 : 0.015;
+    //
+    //     const takerFee = Number(
+    //         (listingPriceInSol * takerFeePercentage).toFixed(priceDecimals)
+    //     );
+    //
+    //     const royaltyFee = Number(
+    //         (listingPriceInSol * ROYALTY_FEE).toFixed(priceDecimals)
+    //     );
+    //
+    //     const priceWithRoyalties = (
+    //         Number(listingPriceInSol) +
+    //         takerFee +
+    //         royaltyFee
+    //     ).toFixed(priceDecimals);
+    //
+    //     const {seller, abbreviatedSeller} = extractSeller(requestBody);
+    //
+    //     const sellerUrl = `https://solana.fm/address/${seller}`;
+    //
+    //     const solPrice = await CoingeckoController.fetchSolPrice();
+    //
+    //     const priceWithRoyaltiesInUSD = (
+    //         Number(priceWithRoyalties) * solPrice
+    //     ).toFixed(0);
+    //
+    //     const hegends = await CoingeckoController.getHegends();
+    //
+    //     const floorPrice = hegends.floor_price.native_currency;
+    //     const marketCapSol = Math.round(floorPrice * 2222);
+    //
+    //     const floorPriceInUSD = (floorPrice * solPrice).toFixed(0);
+    //     const marketCapInUSD = addCommas(Math.round(marketCapSol * solPrice));
+    //
+    //     const volume24h = hegends.volume_24h.native_currency;
+    //     const volume24hInUSD = (volume24h * solPrice).toFixed(0);
+    //
+    //     let messageToSendTransfer;
+    //     // const messageToSendTransfer = `
+    //     // <b>New ${action}!</b>\n\n<b>${name}</b>\n${desc}\n\n<b>Market:</b> <a href='${url}'>${mp}</a>\n<b>Rank: </b>${rank}\n<b>Tier: </b>${tier}\n<b>Price: </b>${priceWithRoyalties} SOL ($${priceWithRoyaltiesInUSD})\n\n<b>Floor Price: </b>${floorPrice.toFixed(2)} SOL ($${floorPriceInUSD})\n<b>Volume 24h: </b>${volume24h} SOL ($${volume24hInUSD})\n<b>Market Cap: </b>${marketCapSol.toFixed(0)} SOL ($${marketCapInUSD})\n\n<a href='${Transfersignature}'>TX</a> | <a href='${mintUrl}'>Mint</a> | <a href='${sellerUrl}'>Seller</a>`;
+    //
+    //     if (action === "Listing") {
+    //       // await sendToTelegramNFT(messageToSendTransfer, im);
+    //     } else if (action === "Sell") {
+    //
+    //       const data = {
+    //         uri: im,
+    //         text: `New Hegend Buy!\n\n${name}\n${desc}\n\nRank: ${rank}\nTier: ${tier}\nPrice: ${priceWithRoyalties} SOL (\$${priceWithRoyaltiesInUSD})\n${url}`,
+    //       };
+    //
+    //       try {
+    //         const tweet = await TwitterController.postToTwitter(data);
+    //         console.log('Tweet posted:', tweet);
+    //         console.log('Tweet posted:', tweet.data.id);
+    //         const tweetURL = `https://x.com/${X_USER}/status/${tweet.data.id}`
+    //         messageToSendTransfer = `<b>New ${action}!</b>\n\n<b>${name}</b>\n${desc}\n\n<b>Raid Tweet</b>\n${tweetURL}\n\n<b>Market:</b> <a href='${url}'>${mp}</a>\n<b>Rank: </b>${rank}\n<b>Tier: </b>${tier}\n<b>Price: </b>${priceWithRoyalties} SOL ($${priceWithRoyaltiesInUSD})\n\n<b>Floor Price: </b>${floorPrice.toFixed(2)} SOL ($${floorPriceInUSD})\n<b>Volume 24h: </b>${volume24h} SOL ($${volume24hInUSD})\n<b>Market Cap: </b>${marketCapSol.toFixed(0)} SOL ($${marketCapInUSD})\n\n<a href='${Transfersignature}'>TX</a> | <a href='${mintUrl}'>Mint</a> | <a href='${sellerUrl}'>Seller</a>`;
+    //
+    //       } catch (error) {
+    //         messageToSendTransfer = `
+    //     <b>New ${action}!</b>\n\n<b>${name}</b>\n${desc}\n\n<b>Market:</b> <a href='${url}'>${mp}</a>\n<b>Rank: </b>${rank}\n<b>Tier: </b>${tier}\n<b>Price: </b>${priceWithRoyalties} SOL ($${priceWithRoyaltiesInUSD})\n\n<b>Floor Price: </b>${floorPrice.toFixed(2)} SOL ($${floorPriceInUSD})\n<b>Volume 24h: </b>${volume24h} SOL ($${volume24hInUSD})\n<b>Market Cap: </b>${marketCapSol.toFixed(0)} SOL ($${marketCapInUSD})\n\n<a href='${Transfersignature}'>TX</a> | <a href='${mintUrl}'>Mint</a> | <a href='${sellerUrl}'>Seller</a>`;
+    //
+    //         console.error('X Error:', error);
+    //       }
+    //
+    //       if (priceWithRoyalties >= 1) {
+    //         await sendToTelegramNFT(messageToSendTransfer, im);
+    //       }
+    //
+    //     } else {
+    //       console.log("Transfer Transaction:");
+    //     }
+    //   }
+    //   res.status(200).send("Logged POST request body.");
+    // } else {
+    //   res.status(405).send("Method not allowed.");
+    // }
   }
 };
 
